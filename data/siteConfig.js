ce3bbe3c2b68b5556532ec5236c1ba3a94df44aa
jonathan-hetrafi <jonathan@hetrafi.com>
2022-01-26 21:26:36 +0100

    init

---
+++ data/siteConfig.js
@@ -0,0 +1,214 @@
+module.exports = {
+  siteTitle: 'Hi! I\'m Jonathan!',
+  siteDescription: `Create your online curriculum in just a few minutes with this starter`,
+  keyWords: ['gatsbyjs', 'react', 'curriculum'],
+  authorName: 'Jonathan Laliberte',
+  stackOverflowUsername: '5360672',
+  githubUsername: 'jonathanlal',
+  authorAvatar: '/images/avatar.jpg',
+  authorDescription: `Conscientious, creative and adaptable individual with high drive and initiative.
+Excellent team player who can be trusted with delegated responsibility and is
+able to focus on set objectives, as well as multi-task, while working in
+pressurized environments. <br ><br >
+Seeks to commit to a position with growth potential over the long-term.
+Full-time work is preferable and is also willing to relocate.`,
+  skills: [
+    {
+      name: 'Java',
+      level: 87
+    },
+    {
+      name: 'Javascript',
+      level: 85
+    },
+    {
+      name: 'PHP',
+      level: 55
+    },
+    {
+      name: 'SQL',
+      level: 70
+    },
+    {
+      name: 'C#',
+      level: 55
+    },
+    {
+      name: 'Python',
+      level: 40
+    },
+    {
+      name: 'Go',
+      level: 40
+    },
+    {
+      name: 'C++',
+      level: 30
+    },
+    {
+      name: 'HTML/CSS',
+      level: 95
+    },
+    {
+      name: 'React',
+      level: 81
+    },
+    {
+      name: 'Angular',
+      level: 76
+    },
+    {
+      name: 'Next.js',
+      level: 84
+    },
+
+
+
+    {
+      name: 'Git',
+      level: 80
+    },
+
+
+    {
+      name: 'Google Cloud Platform',
+      level: 70
+    },
+    {
+      name: 'Azure',
+      level: 65
+    },
+
+    /* more skills here */
+  ],
+  jobs: [
+    /* more jobs here */
+    {
+      company: "Domus Venari",
+      begin: {
+        month: 'JAN',
+        year: '2022'
+      },
+      duration: 'Present',
+      occupation: "Lead developer",
+      description: `Currently building a new site for a real estate agency together with two other developers`,
+    },
+    {
+      company: "Hetrafi",
+      begin: {
+        month: 'NOV',
+        year: '2021'
+      },
+      duration: '1 yr 1 month',
+      occupation: "Senior Fullstack Developer/Project Manager",
+      description: `Hetrafi was a startup play-to-earn gaming platform. They originally had a team in India who built their site, however it was unmanageable. 
+      We were creating a new site while maintaining the old one, I was in charge or recruiting, DevOps, coordinating between team members, code review and choosing what technologies we would use. Unfortunately a couple months before completing the site, the company ran out of funds.`,
+    },  {
+      company: "Cash-for-gold",
+      begin: {
+        month: 'DEC',
+        year: '2020'
+      },
+      duration: '8 months',
+      occupation: "Fullstack developer",
+      description: "Cash-for-gold.co.uk was a company offering to buy gold online over mail. I created a bespoke CRM and website for them."
+    }, {
+      company: "Domus Venari",
+      begin: {
+        month: 'Feb',
+        year: '2019'
+      },
+      duration: '1 yr 3 months',
+      occupation: "Fullstack developer",
+      description: "Developing, implementing and monitoring solutions for in-house CRM. Creating landing pages for marketing initiatives. Assistant network manager for 60 networked computers over three offices."
+    }, {
+      company: "Wistla",
+      begin: {
+        month: 'SEP',
+        year: '2017'
+      },
+      duration: '7 months',
+      occupation: "Front end developer",
+      description: "Developing web pages following design concepts from design team."
+    },
+    {
+      company: "Diversified Resorts",
+      begin: {
+        month: 'Aug',
+        year: '2016'
+      },
+      duration: '1 yr 5 months',
+      occupation: "Front end developer",
+      description: "Developing landing pages and making design changes to their website"
+    },
+    {
+      company: "Rooftop Cafe",
+      begin: {
+        month: 'JUN',
+        year: '2015'
+      },
+      duration: '6 months',
+      occupation: "Kitchen Porter",
+      description: "(part-time) Washing dishes, preparing desserts/appetizers & stock upkeep"
+    },
+    {
+      company: "MailBoxes Etc",
+      begin: {
+        month: 'Nov',
+        year: '2014'
+      },
+      duration: '1 year',
+      occupation: "Store Clerk",
+      description: "(part-time) Dealing with mail services: domestic and international. Packaging & Cashier."
+    },
+    {
+      company: "Sunset Beach Club",
+      begin: {
+        month: 'Mar',
+        year: '2011/2'
+      },
+      duration: 'Summers',
+      occupation: "IT support",
+      description: "(part-time) Shadowing head of department in computer repairs, software updating and other computer related issues experienced by department staff on a day to day basis."
+    },
+  ],
+  portfolio: [
+    {
+      image: "/images/gatsby-starter-cv.png",
+      description: "CV: Jonathan Laliberte",
+      url: "https://www.gatsbyjs.org/starters/santosfrancisco/gatsby-starter-cv/"
+    },
+    {
+      image: "/images/awesome-grid.png",
+      description: "Responsive grid for ReactJS",
+      url: "https://github.com/santosfrancisco/react-awesome-styled-grid"
+    },
+    /* more portifolio items here */
+  ],
+  social: {
+    stackoverflow: "https://stackoverflow.com/users/5360672/jonathan-laliberte",
+    linkedin: "https://www.linkedin.com/in/jonathan-laliberte/",
+    github: "https://github.com/jonathanlal",
+    email: "jonathanmlaliberte@gmail.com"
+  },
+  siteUrl: 'https://santosfrancisco.github.io/gatsbystarter-cv',
+  pathPrefix: '/gatsby-starter-cv', // Note: it must *not* have a trailing slash.
+  siteCover: '/images/cover.jpeg',
+  googleAnalyticsId: 'UA-000000000-1',
+  background_color: '#ffffff',
+  theme_color: '#25303B',
+  fontColor: "#000000cc",
+  enableDarkmode: true, // If true, enables dark mode switch
+  display: 'minimal-ui',
+  icon: 'src/assets/gatsby-icon.png',
+  headerLinks: [
+    {
+      label: 'Home',
+      url: '/',
+    },
+    {
+      label: 'Portfolio',
+      url: '/portfolio',
+    }
+  ]
+}
